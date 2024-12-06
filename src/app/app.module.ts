3afed66ff7bce85f1b0b701bad3aba1b2f74cbe9
cleiton valentim <cleiton_valentim@hotmail.com.br>
2024-12-06 11:28:14 -0300

    Componentes criado

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { TableComponent } from './table/table.component';
+import { FormComponent } from './form/form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    TableComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
