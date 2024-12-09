97b007b59c6c1f0dafa93a340843fbb52ebd6d70
cleiton valentim <cleiton_valentim@hotmail.com.br>
2024-12-09 09:07:18 -0300

    interface criada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TableComponent } from './table/table.component';
+import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'/home', pathMatch:'full'},
+  {path: 'home', component: HomeComponent},
+  {path:'compromisso', component: TableComponent},
+  {path: 'compromisso/:id', component: FormComponent},
+  {path: 'compromisso', component: FormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/compromisso.ts
@@ -0,0 +1,10 @@
+export interface Compromisso {
+  id:number;
+  title:string;
+  descricao: string;
+  datacompromisso: string;
+  inicio: string;
+  fim:string;
+  local:string;
+  situacao:string;
+}
